a4fd043c6415d93439e3acf049c5d4c811b9c9d6
Morkros <102390567+Morkros@users.noreply.github.com>
2023-04-14 21:16:46 -0300

    Add files via upload

---
+++ TP 1/JS/funciones.js
@@ -0,0 +1,7 @@
+console.log("funsionando (kassssi) al 100%");  
+function nombreEdad(){
+    const aux=document.getElementById("cajaTexto1");
+    const aux1=document.getElementById("cajaTexto2");
+    const aux2=document.getElementById("cajaTexto3");
+        alert(aux.value + ", " + aux1.value + ", " + aux2.value + " años");
+}
